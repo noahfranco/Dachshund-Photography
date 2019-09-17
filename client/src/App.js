f0c17b5586bd57e32ee5e65bec50dc41536c0b7f
noahfranco <noahfra@gmail.com>
2019-09-17 01:06:05 -0400

    updated some styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import GetData from "./components/GetData.js"; 
 import Styled from "styled-components"; 
 
-const Color = Styled.div`
-background: gray; 
+const Background = Styled.div`
+background-image: url("https://wallpaperbro.com/img/124891.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center; 
 `
 
 function App() {
   return (
+    <Background> 
     <div className="App">
       <h1> Dachshund Gallery </h1> 
       <GetData /> 
     </div>
+    </Background>
   );
 }
 
 export default App;
+
